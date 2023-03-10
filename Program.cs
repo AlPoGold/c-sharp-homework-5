38e884507f19e8b75d4b5ea6e3062452b8394bfd
Polina Kolesnikova <polina@example.com>
2023-03-10 15:57:10 +0300

    adding exercise 36

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+// Console.Write("Please, enter size of your array: ");
+// int size = int.Parse(Console.ReadLine());
+// int[] myArray = new int[size];
+// int count = 0;
+// for(int i = 0; i < size; i++)
+// {
+//     myArray[i] = new Random().Next(100, 1000);
+//     if (myArray[i]%2 ==0) count++;
+
+    
+// }
+// for(int i = 0; i < size;i++) Console.Write($"{myArray[i]} ");
+// Console.WriteLine();
+// Console.WriteLine($"The quantity of even numbers in your array {count}");
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Please, enter size of your array: ");
+int size = int.Parse(Console.ReadLine());
+int[] myArray = new int[size];
+int sum = 0;
+
+for(int i = 0; i < size; i++)
+{
+    myArray[i] = new Random().Next();
+    if (i%2 == 1) sum += myArray[i];
+
+    
+}
+Console.Write($"Your array is {String.Join(", ", myArray)} ");
+Console.WriteLine();
+Console.WriteLine($"The summary of numbers on odd position in your array {sum}");
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
